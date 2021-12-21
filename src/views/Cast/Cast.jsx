42ba57c0b66726dc8bac2d90d8f3ea9b1f010b29
Унжакова Екатерина <katerina.unz@gmail.com>
2021-12-21 17:32:51 +0700

    Дебагинг Cast

--- src/views/Cast/Cast.jsx
+++ src/views/Cast/Cast.jsx
@@ -3,7 +3,7 @@ import { fetchMovieCasts } from '../../services/tmdbfilms-api';
 import PropTypes from 'prop-types';
 import s from './Cast.module.css';
 import { toast } from 'react-toastify';
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 
 export default function Cast({ movieId }) {
   const defaultImg = 'https://socialkit.ru/thumbs/crop/406x558/no-image.jpg';
@@ -25,30 +25,32 @@ export default function Cast({ movieId }) {
         }),
       );
   }, [movieId]);
-  <>
-    {cast.length > 0 ? (
-      <ul>
-        {cast.map(({ id, profile_path, name, character }) => (
-          <li key={id} className={s.CastItem}>
-            <img
-              src={
-                profile_path
-                  ? `https://image.tmdb.org/t/p/w300/${profile_path}`
-                  : defaultImg
-              }
-              alt={name}
-              className={null}
-              width="150"
-            />
-            <p>{name}</p>
-            <p>{character}</p>
-          </li>
-        ))}
-      </ul>
-    ) : (
-      <p>No found</p>
-    )}
-  </>;
+  return (
+    <>
+      {cast && cast.length > 0 ? (
+        <ul>
+          {cast.map(({ id, profile_path, name, character }) => (
+            <li key={id} className={s.CastItem}>
+              <img
+                src={
+                  profile_path
+                    ? `https://image.tmdb.org/t/p/w300/${profile_path}`
+                    : defaultImg
+                }
+                alt={name}
+                className={null}
+                width="150"
+              />
+              <p>{name}</p>
+              <p>{character}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No found</p>
+      )}
+    </>
+  );
 }
 
 Cast.propTypes = { movieId: PropTypes.string.isRequired };
--- src/views/MoviesPage/MoviesPage.jsx
+++ src/views/MoviesPage/MoviesPage.jsx
@@ -7,6 +7,37 @@ import s from './MoviesPage.module.css';
 
 const MoviesPage = () => {
   const defaultImg = 'https://socialkit.ru/thumbs/crop/406x558/no-image.jpg';
+  const [movies, setMovies] = useState([]);
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    fetchSearchingMovies()
+      .then(data => data.results)
+      .then(results =>
+        results.map(({ id, name, original_title, poster_path }) => {
+          return {
+            id,
+            name,
+            original_title,
+            poster_path,
+          };
+        }),
+      )
+      .then(setMovies)
+      .catch(error =>
+        toast.error('Error, try again later', {
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        }),
+      );
+    console.log(movies);
+  }, []);
 
   return (
     <div>
