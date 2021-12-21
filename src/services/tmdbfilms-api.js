af1ff017ab088903d39dd7ce51dea1bc6b006224
Унжакова Екатерина <katerina.unz@gmail.com>
2021-12-21 11:56:38 +0700

    Update tmdbfilms-api.js

--- src/services/tmdbfilms-api.js
+++ src/services/tmdbfilms-api.js
@@ -8,14 +8,32 @@ async function fetchWithErrorHandling(url = '', config = {}) {
     : Promise.reject(new Error('Not found'));
 }
 
-export function fetchAuthors() {
-  return fetchWithErrorHandling(`${BASE_URL}/authors?_embed=books`);
+export function fetchTrendingMovies() {
+  return fetchWithErrorHandling(
+    `${BASE_URL}/trending/movie/day?api_key=${KEY}`,
+  );
 }
 
-export function fetchBooks() {
-  return fetchWithErrorHandling(`${BASE_URL}/books`);
+export function fetchSearchingMovies(search) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}/search/movie?api_key=${KEY}&query=${search}&include_adult=false&language=en-US`,
+  );
 }
 
-export function fetchBookById(bookId) {
-  return fetchWithErrorHandling(`${BASE_URL}/books/${bookId}?_expand=author`);
+export function fetchMovieDetails(movieId) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}/movie/${movieId}?api_key=${KEY}&language=en-US`,
+  );
+}
+
+export function fetchMovieCredits(movieId) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}/movie/${movieId}/credits?api_key=${KEY}&language=en-US`,
+  );
+}
+
+export function fetchMovieReviews(movieId) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}/movie/${movieId}/reviews?api_key=${KEY}&language=en-US&page=1`,
+  );
 }
