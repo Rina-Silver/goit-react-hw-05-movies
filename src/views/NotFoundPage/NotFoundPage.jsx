ada00f95f9698affba2673cc5ab6b8a449c476e5
Унжакова Екатерина <katerina.unz@gmail.com>
2022-01-13 02:03:48 +0700

    Update NotFoundPage.jsx

--- src/views/NotFoundPage/NotFoundPage.jsx
+++ src/views/NotFoundPage/NotFoundPage.jsx
@@ -1,8 +1,10 @@
 // import s from './NotFoundPage.module.css';
+import { ReactComponent as Browser404 } from '../../img/404browser_102160.svg';
+
 export default function NotFoundPage() {
   return (
     <>
-      <h2>Page not found</h2>
+      <Browser404 />
     </>
   );
 }
